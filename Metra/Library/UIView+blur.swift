a481b83f5962e3c45d7f4bc74ed437990945e4a6
Alexandre Camilleri <alexcamill@gmail.com>
2020-12-07 17:10:10 +0800

    Metrics
    Uses combine, added SnapKit also and some other tuning to the rendering

--- Metra/Library/UIView+blur.swift
+++ Metra/Library/UIView+blur.swift
@@ -12,15 +12,19 @@ extension UIView {
     
     /// Add a transparency effect to the view - Only if user hasn't disabled them
     /// - Parameter style: The style of the blur effect, by default `.systemThinMaterial`
-    func addBlurEffectView(style: UIBlurEffect.Style = .systemThinMaterial) {
-        guard !UIAccessibility.isReduceTransparencyEnabled else { return }
-        backgroundColor = .clear
+    @discardableResult
+    func addBlurEffectView(style: UIBlurEffect.Style = .systemUltraThinMaterialLight) -> UIVisualEffectView? {
+        guard !UIAccessibility.isReduceTransparencyEnabled else {
+            return nil
+        }
         let blurEffect = UIBlurEffect(style: style)
         let blurEffectView = UIVisualEffectView(effect: blurEffect)
         
+        backgroundColor = .clear
+        addSubview(blurEffectView)
         blurEffectView.snp.makeConstraints { (make) in
             make.edges.equalToSuperview()
         }
-        addSubview(blurEffectView)
+        return blurEffectView
     }
 }
