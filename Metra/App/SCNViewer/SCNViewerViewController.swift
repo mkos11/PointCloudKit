5b800ccd13f3928dfbf47bb72637a8fe63a66945
Alexandre Camilleri <alexcamill@gmail.com>
2020-12-14 11:35:31 +0800

    Little cleanup Proper controls for view/export  - Moved export to viewer Remove hacky view bar

--- Metra/App/SCNViewer/SCNViewerViewController.swift
+++ Metra/App/SCNViewer/SCNViewerViewController.swift
@@ -5,24 +5,26 @@
 //  Created by Alexandre Camilleri on 11/12/2020.
 //
 
+import UIKit
+import Combine
 import SceneKit
 import SnapKit
 
 final class SCNViewerViewController: UIViewController {
+    private var cancellable: Set<AnyCancellable> = []
 
     private let viewModel: SCNViewerViewModel
     
+    private let controlsStackView = UIStackView()
+    private let activityIndicatorView = UIActivityIndicatorView()
     private let sceneView = SCNView()
+    private let documentInteractionController = UIDocumentInteractionController()
     
     // Auto-hide the home indicator to maximize immersion in AR experiences.
-    override var prefersHomeIndicatorAutoHidden: Bool {
-        return true
-    }
+    override var prefersHomeIndicatorAutoHidden: Bool { true }
     
     // Hide the status bar to maximize immersion in AR experiences.
-    override var prefersStatusBarHidden: Bool {
-        return true
-    }
+    override var prefersStatusBarHidden: Bool { true }
     
     init(viewModel: SCNViewerViewModel) {
         self.viewModel = viewModel
@@ -36,7 +38,8 @@ final class SCNViewerViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
-        loadScene()
+        setupControls()
+        setupObservers()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -44,41 +47,31 @@ final class SCNViewerViewController: UIViewController {
         navigationController?.setNavigationBarHidden(false, animated: animated)
     }
     
-    private func setupUI() {
-        view.addSubview(sceneView)
-        sceneView.snp.makeConstraints { (make) in
-            make.edges.equalTo(view.safeAreaInsets)
-        }
-    }
-    
-    private func loadScene() {
+    private func load(scene: SCNScene) {
         // 1: Load .scn file
-        let scene = SCNSceneSource(url: viewModel.scnFileLocation)?.scene()
+//        let scene = SCNSceneSource(url: viewModel.scnFileLocation)?.scene()
         
         // 2: Add camera node
         let cameraNode = SCNNode()
         cameraNode.camera = SCNCamera()
         // 3: Place camera
-        cameraNode.position = SCNVector3(x: 0, y: 10, z: 35)
+        cameraNode.position = SCNVector3(x: 0, y: 1, z: 5)
         // 4: Set camera on scene
-        scene?.rootNode.addChildNode(cameraNode)
+        scene.rootNode.addChildNode(cameraNode)
         
-        // 5: Adding light to scene
-        let lightNode = SCNNode()
-        lightNode.light = SCNLight()
-        lightNode.light?.type = .omni
-        lightNode.position = SCNVector3(x: 0, y: 10, z: 35)
-        scene?.rootNode.addChildNode(lightNode)
-        
-        // 6: Creating and adding ambien light to scene
+//        // 5: Adding light to scene
+//        let lightNode = SCNNode()
+//        lightNode.light = SCNLight()
+//        lightNode.light?.type = .omni
+//        lightNode.position = SCNVector3(x: 0, y: 0, z: 40)
+//        scene.rootNode.addChildNode(lightNode)
+
+        // 6: Creating and adding ambient light to scene
         let ambientLightNode = SCNNode()
         ambientLightNode.light = SCNLight()
         ambientLightNode.light?.type = .ambient
         ambientLightNode.light?.color = UIColor.darkGray
-        scene?.rootNode.addChildNode(ambientLightNode)
-        
-        // If you don't want to fix manually the lights
-//        sceneView.autoenablesDefaultLighting = true
+        scene.rootNode.addChildNode(ambientLightNode)
         
         // Allow user to manipulate camera
         sceneView.allowsCameraControl = true
@@ -94,5 +87,59 @@ final class SCNViewerViewController: UIViewController {
         
         // Set scene settings
         sceneView.scene = scene
+        
+        sceneView.alpha = 1
+    }
+    
+    @objc
+    private func export() {
+        documentInteractionController.url = viewModel.exportUrl
+        documentInteractionController.uti = viewModel.exportUti
+        documentInteractionController.name = viewModel.filename
+        documentInteractionController.presentOptionsMenu(from: view.frame,
+                                                         in: view,
+                                                         animated: true)
+    }
+}
+
+extension SCNViewerViewController {
+    
+    private func setupUI() {
+        view.backgroundColor = UIColor.black
+        // Scene view
+        view.addSubview(sceneView)
+        sceneView.snp.makeConstraints { (make) in
+            make.edges.equalTo(view.safeAreaInsets)
+        }
+        sceneView.alpha = 0
+        // Activity indicator view
+        activityIndicatorView.style = .large
+        activityIndicatorView.color = .darkGray
+        view.addSubview(activityIndicatorView)
+        activityIndicatorView.snp.makeConstraints { (make) in
+            make.center.equalTo(view.center)
+        }
+    }
+    
+    private func setupObservers() {
+        // Wait for scene to be ready and loads it
+        activityIndicatorView.startAnimating()
+        viewModel.scenePublisher
+            .sink { [unowned self] (scene) in
+                DispatchQueue.main.async {
+                    self.activityIndicatorView.stopAnimating()
+                    UIView.animate(withDuration: 1) {
+                        self.load(scene: scene)
+                    }
+                }
+            }
+            .store(in: &cancellable)
+    }
+    
+    private func setupControls() {
+        // Export button
+        let exportCaptureButton = UIBarButtonItem(title: "Save", style: .plain,
+                                                  target: self, action: #selector(export))
+        navigationItem.rightBarButtonItem = exportCaptureButton
     }
 }
--- Metra/App/SCNViewer/SCNViewerViewModel.swift
+++ Metra/App/SCNViewer/SCNViewerViewModel.swift
@@ -6,11 +6,29 @@
 //
 
 import Foundation
+import SceneKit
+import Combine
 
 final class SCNViewerViewModel {
-    let scnFileLocation: URL
+    private lazy var temporaryDirectoryUrl = URL(fileURLWithPath: NSTemporaryDirectory(), isDirectory: true)
     
-    init(scnFileLocation: URL) {
-        self.scnFileLocation = scnFileLocation
+    lazy var filename = "metra3dfile\(UUID().uuidString)"
+    lazy var exportUrl = temporaryDirectoryUrl.appendingPathComponent("\(filename).scn")
+    let exportUti = "public.data, public.content"
+    
+    let scenePublisher: PassthroughSubject<SCNScene, Never>
+    
+    init(scenePublisher: PassthroughSubject<SCNScene, Never>) {
+        self.scenePublisher = scenePublisher
+    }
+    
+    func writeScene(scene: SCNScene) {
+        scene.write(to: exportUrl,
+                    options: nil,
+                    delegate: nil) { (progress, error, _) in
+            if let error = error {
+                fatalError(error.localizedDescription)
+            }
+        }
     }
 }
