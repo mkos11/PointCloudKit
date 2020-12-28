e662bb87a0b4fa0ee032e7775e66cbc3fbdf1bfb
Alexandre Camilleri <alexcamill@gmail.com>
2020-12-28 09:42:51 +0800

    Fastlane Update git ignore, removed App Store screen shot from the project as they are managed by Fastlane fastlane init swift https://docs.fastlane.tools/actions/

---
+++ fastlane/Appfile.swift
@@ -0,0 +1,9 @@
+var appIdentifier: String { return "com.metra.Metra" } // The bundle identifier of your app
+var appleID: String { return "raydonovan01@protonmail.com" } // Your Apple email address
+
+var itcTeam: String? { return "96032919" } // App Store Connect Team ID
+var teamID: String { return "H2694FBVFN" } // Apple Developer Portal Team ID
+
+
+// For more information about the Appfile, see:
+//     https://docs.fastlane.tools/advanced/#appfile
